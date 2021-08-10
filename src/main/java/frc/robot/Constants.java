997d20f13fc838854f7b1681316777a7eaa0343b
Ryan <ryanpcather@gmail.com>
2021-08-10 01:06:05 -0400

    finished all teleop drive components

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,22 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+        public static final int ID_LEFT_PRIMARY = 0;  
+        public static final int ID_LEFT_SECONDARY = 0;  
+        public static final int ID_RIGHT_PRIMARY = 0;  
+        public static final int ID_RIGHT_SECONDARY = 0;  
+
+        public static final Double speed = 1.0;
+    
+
+   
+        public static final int JOYSTICK_PORT = 0;  
+
+        public static final int LEFT_AXIS = 0;  
+        public static final int RIGHT_AXIS = 0;  
+    
+    
+
+}
